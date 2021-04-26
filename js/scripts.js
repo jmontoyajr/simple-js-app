fcf84c1c2a2e6fee15b8b0c742d093420d4b058a
jmontoyajr <82304971+jmontoyajr@users.noreply.github.com>
2021-04-26 13:34:34 -0700

    Update scripts.js
    - updated array with bracket

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,5 @@
 let pokemonList = [
     {name: "Bulbasaur", height: 0.7, type: ["grass", "poison"]},
     {name: "Charizard", height: 1.7, type: ["fire", "flying"]},
-    {name: "Pikachu", height: 0.4, type: "electric"},
+    {name: "Pikachu", height: 0.4, type: ["electric"]},
 ];
