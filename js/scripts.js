ea638afe9cd83ec54d984dd4c80a39c12f4ea072
jmontoyajr <82304971+jmontoyajr@users.noreply.github.com>
2021-04-25 21:48:39 -0700

    Update scripts.js
    - Deleted old scripts
    - Added Pokemon Array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,2 @@
-alert('Hello World');
-
-let favoriteFood = 'Tacos';
-document.write(favoriteFood);
+// Pokemon Array
+let pokemonList = [{name: "Bulbasaur", height: 0.7, type: ["grass", "poison"]}{name: "Charizard", height: 1.7, type: ["fire", "flying"]}{name: "Pikachu", height: 0.4, type: "electric"}];
