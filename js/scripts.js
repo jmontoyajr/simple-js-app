126567a7b0796eafafed300937804b2559dd3910
jmontoyajr <82304971+jmontoyajr@users.noreply.github.com>
2021-04-25 21:51:37 -0700

    Update scripts.js
    - added missing commas to array

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,2 @@
 // Pokemon Array
-let pokemonList = [{name: "Bulbasaur", height: 0.7, type: ["grass", "poison"]}{name: "Charizard", height: 1.7, type: ["fire", "flying"]}{name: "Pikachu", height: 0.4, type: "electric"}];
+let pokemonList = [{name: "Bulbasaur", height: 0.7, type: ["grass", "poison"]},{name: "Charizard", height: 1.7, type: ["fire", "flying"]},{name: "Pikachu", height: 0.4, type: "electric"}];
