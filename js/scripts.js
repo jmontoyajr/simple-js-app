2bb83fe2d1953c9201c86db3edbb616666d21d6c
jmontoyajr <82304971+jmontoyajr@users.noreply.github.com>
2021-04-29 01:06:31 -0700

    Update scripts.js
    - Added html formatting to document.write output.

--- js/scripts.js
+++ js/scripts.js
@@ -8,8 +8,8 @@ let pokemonList = [
 // Initializes for loop to start at index of array, goes through the entire array to find the heights of each Pokemon
 for (let i = 0; i < pokemonList.length; i++) {
     if (pokemonList[i].height > 1) {
-    document.write(pokemonList[i].name + " (height:) " + pokemonList[i].height + ". Wow, that's big! ");
+    document.write(pokemonList[i].name + " (height:) " + pokemonList[i].height + "<strong>. Wow, that's big! </strong><br><br>");
   } else {
-    document.write(pokemonList[i].name + " (height:) " +  pokemonList[i].height + ", is small. ")
+    document.write(pokemonList[i].name + " (height:) " +  pokemonList[i].height + "<i>, is small. </i><br><br>")
   }
 }
