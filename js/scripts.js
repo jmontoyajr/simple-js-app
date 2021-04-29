4b3a7cd203fa0c87d76529586433f281f89d0050
jmontoyajr <82304971+jmontoyajr@users.noreply.github.com>
2021-04-29 00:47:18 -0700

    Update scripts.js
    - Updated to include (height:) as a string concatenated to the printed items

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,11 @@ let pokemonList = [
     {name: "Pikachu", height: 0.4, type: ["electric"]},
 ];
 
+// Initializes for loop to start at index of array, goes through the entire array to find the heights of each Pokemon
 for (let i = 0; i < pokemonList.length; i++) {
     if (pokemonList[i].height > 1) {
-    document.write(pokemonList[i].name + " wow, that's big! ");
+    document.write(pokemonList[i].name + " (height:) " + pokemonList[i].height + " wow, that's big! ");
   } else {
-    document.write(pokemonList[i].name + " is small. ")
+    document.write(pokemonList[i].name + " (height:) " +  pokemonList[i].height + " is small. ")
   }
 }
