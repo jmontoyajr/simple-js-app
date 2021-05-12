15e7f882fc730b640eb65ca14b157827f4235220
jmontoyajr <82304971+jmontoyajr@users.noreply.github.com>
2021-05-12 00:33:39 -0700

    Update scripts.js
    - Create new pokemonList Array
    - Created forEach loop to print each pokemon's information.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,38 @@
-// Pokemon Array
+
+// Creates array with list of pokemons
+let pokemonList = [
+  {
+    name: 'Bulbasaur',
+    height: 0.7,
+    type: ['grass', 'poison']
+  },
+  {
+    name: 'Charizard',
+    height: 1.7,
+    type: ['fire', 'flying']
+  },
+  {
+    name: 'Pikachu',
+    height: 0.4,
+    type: ['electric']
+  }
+];
+
+// Creates forEach Loop that passes pokemon function for each element in the array
+pokemonList.forEach(function(pokemon) {
+  document.write(pokemon.name + ' is ' + pokemon.height + ' m tall <br><br>');
+});
+
+
+/* Pokemon Array ORIGINAL CODE for reference
 let pokemonList = [
     {name: "Bulbasaur", height: 0.7, type: ["grass", "poison"]},
     {name: "Charizard", height: 1.7, type: ["fire", "flying"]},
     {name: "Pikachu", height: 0.4, type: ["electric"]},
 ];
+*/
 
-// Initializes for loop to start at index of array, goes through the entire array to find the heights of each Pokemon
+/* Initializes for loop to start at index of array, goes through the entire array to find the heights of each Pokemon - ORIGINAL CODE for reference
 for (let i = 0; i < pokemonList.length; i++) {
     if (pokemonList[i].height > 1) {
     document.write(pokemonList[i].name + " (height:) " + pokemonList[i].height + "<strong>. Wow, that's big! </strong><br><br>");
@@ -13,3 +40,4 @@ for (let i = 0; i < pokemonList.length; i++) {
     document.write(pokemonList[i].name + " (height:) " +  pokemonList[i].height + "<i>, is small. </i><br><br>")
   }
 }
+*/
