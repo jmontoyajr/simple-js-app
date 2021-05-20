d0402e9051702f659d92a516d1d9b03d808154da
jmontoyajr <82304971+jmontoyajr@users.noreply.github.com>
2021-05-19 17:42:21 -0700

    Update scripts.js
    - removed old comments

--- js/scripts.js
+++ js/scripts.js
@@ -51,38 +51,3 @@ pokemonRepository.getAll().forEach(function(pokemon) {
   pokemonRepository.add({ height: 1 });
   pokemonRepository.add({ type: ["Ground"] });
 })
-
-// OLD CODE FOR REFERENCE
-/* Object key approach
-
-Object.keys(pokemonRepository).forEach(function(pokemonList) {
-  document.write(pokemonRepository[pokemonList]);
-  console.log(pokemonRepository.getAll());
-});
-
-*/
-
-/* Initial forEach coding approach
-pokemonRepository.forEach(function(getAll) {
-  document.write(pokemonList.name + ' is ' + pokemonList.height + ' m tall ' + ' and uses ' + pokemonList.type);
-});
-*/
-
-
-/* Pokemon Array ORIGINAL CODE for reference
-let pokemonList = [
-    {name: "Bulbasaur", height: 0.7, type: ["grass", "poison"]},
-    {name: "Charizard", height: 1.7, type: ["fire", "flying"]},
-    {name: "Pikachu", height: 0.4, type: ["electric"]},
-];
-*/
-
-/* Initializes for loop to start at index of array, goes through the entire array to find the heights of each Pokemon - ORIGINAL CODE for reference
-for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height > 1) {
-    document.write(pokemonList[i].name + " (height:) " + pokemonList[i].height + "<strong>. Wow, that's big! </strong><br><br>");
-  } else {
-    document.write(pokemonList[i].name + " (height:) " +  pokemonList[i].height + "<i>, is small. </i><br><br>")
-  }
-}
-*/
