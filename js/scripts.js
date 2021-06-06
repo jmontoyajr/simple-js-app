03785cee614dd46df15d17db555ed21d033d0aae
jmontoyajr <82304971+jmontoyajr@users.noreply.github.com>
2021-06-06 11:19:33 -0700

    CSS/Index/JS updates
    - Removed comments in css
    - Removed old #modal-container code
    - Organized html code
    - Added nav bar
    - Linked nav bar items
    - Removed old modal code
    - Added header/main/footer sections
    - Added new list group item class
    - Added new button with modal data-target
    - Added jQuery, Popper.js and Bootstrap JS
    -Removed old modal code
    - Removed class-list.add("button-class")
    - Added jQuery code to generate <li> items
    - Added jQuery code to call the modal

--- js/scripts.js
+++ js/scripts.js
@@ -4,61 +4,6 @@ let pokemonRepository = (function () {
   // Creates new pokemon Array
   let pokemonList = [];
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/';
-  let modalContainer = document.querySelector('#modal-container');
-
-  /* Creates Show Modal function */
-  function showModal(pokemon) {
-    modalContainer.innerHTML = '';
-    let modal = document.createElement('div');
-    modal.classList.add('modal');
-
-    /* Creates close modal functionality */
-    let closeButtonElement = document.createElement('button');
-    closeButtonElement.classList.add('modal-close');
-    closeButtonElement.innerText = 'Close';
-    closeButtonElement.addEventListener('click', hideModal);
-
-    let titleElement = document.createElement('h1');
-    titleElement.innerText = pokemon.name;
-
-    let contentElement = document.createElement('h2');
-    contentElement.innerText = pokemon.height;
-
-    let container = document.querySelector('#image-container');
-    let myImage = document.createElement('img');
-    myImage.src = pokemon.imageUrl;
-
-    /* Calls modal elements */
-    modal.appendChild(closeButtonElement);
-    modal.appendChild(titleElement);
-    modal.appendChild(contentElement);
-    modal.appendChild(myImage);
-    modalContainer.appendChild(modal);
-
-
-    modalContainer.classList.add('is-visible');
-  }
-
-  /* Creates hide modal function */
-  function hideModal() {
-    modalContainer.classList.remove('is-visible');
-  }
-
-  /* Adds event listener to look for keydown or use of Esc key) */
-  window.addEventListener('keydown', (e) => {
-    if (e.key === 'Escape' &&
-    modalContainer.classList.contains('is-visible')) {
-      hideModal();
-    }
-  });
-
-  /* Hides modal if user clicks directly on the overlay */
-  modalContainer.addEventListener('click', (e) => {
-    let target = e.target;
-    if (target === modalContainer) {
-      hideModal();
-    }
-  });
 
   /* Creates function to add pokemon object */
   function add(pokemon) {
@@ -83,16 +28,20 @@ let pokemonRepository = (function () {
   function addListItem(pokemon) {
     let pokemonList = document.querySelector
     (".pokemon-list");
+    let li = $("<li></li>");
+    let inputValue = $("input").val();
+    li.append(inputValue);
     let listPokemon = document.createElement("li");
     let button = document.createElement("button");
     button.innerText = pokemon.name;
-    button.classList.add("button-class");
     listPokemon.appendChild(button);
     pokemonList.appendChild(listPokemon);
     button.addEventListener('click', function(event) {
       showDetails(pokemon);
+      $('#modal').modal(options);
     });
   }
+
 
   /* Load list from pokemon API */
   function loadList() {
