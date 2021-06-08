104fe7b21ac554c21627327c670d5bf05b038c6d
jmontoyajr <82304971+jmontoyajr@users.noreply.github.com>
2021-06-08 01:45:46 -0700

    CSS/Index/JS updates
    CSS
    - Removed comments in css
    - Removed old #modal-container code
    
    HTML
    - Added project links
    - Revised modal structure
    - Revised ul and li code to link to pokemon list
    
    JS
    - Removed old modal code
    - Revised addListItem function to use jQuery to addClass elements to button
    - Added showModal function to define modal elements and display values in modal
    -

--- js/scripts.js
+++ js/scripts.js
@@ -4,61 +4,6 @@ let pokemonRepository = (function () {
   // Creates new pokemon Array
   let pokemonList = [];
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/';
-  let modalContainer = document.querySelector('#modal-container');
-
-  /* Creates Show Modal function */
-  function showModal(pokemon) {
-    modalContainer.innerHTML = '';
-    let modal = document.createElement('div');
-    modal.classList.add('modal');
-
-    /* Creates close modal functionality */
-    let closeButtonElement = document.createElement('button');
-    closeButtonElement.classList.add('modal-close');
-    closeButtonElement.innerText = 'Close';
-    closeButtonElement.addEventListener('click', hideModal);
-
-    let titleElement = document.createElement('h1');
-    titleElement.innerText = pokemon.name;
-
-    let contentElement = document.createElement('h2');
-    contentElement.innerText = pokemon.height;
-
-    let container = document.querySelector('#image-container');
-    let myImage = document.createElement('img');
-    myImage.src = pokemon.imageUrl;
-
-    /* Calls modal elements */
-    modal.appendChild(closeButtonElement);
-    modal.appendChild(titleElement);
-    modal.appendChild(contentElement);
-    modal.appendChild(myImage);
-    modalContainer.appendChild(modal);
-
-
-    modalContainer.classList.add('is-visible');
-  }
-
-  /* Creates hide modal function */
-  function hideModal() {
-    modalContainer.classList.remove('is-visible');
-  }
-
-  /* Adds event listener to look for keydown or use of Esc key) */
-  window.addEventListener('keydown', (e) => {
-    if (e.key === 'Escape' &&
-    modalContainer.classList.contains('is-visible')) {
-      hideModal();
-    }
-  });
-
-  /* Hides modal if user clicks directly on the overlay */
-  modalContainer.addEventListener('click', (e) => {
-    let target = e.target;
-    if (target === modalContainer) {
-      hideModal();
-    }
-  });
 
   /* Creates function to add pokemon object */
   function add(pokemon) {
@@ -83,14 +28,52 @@ let pokemonRepository = (function () {
   function addListItem(pokemon) {
     let pokemonList = document.querySelector
     (".pokemon-list");
+
+    let li = $("<li></li>");
+    let inputValue = $("input").val();
+    li.append(inputValue);
+
     let listPokemon = document.createElement("li");
     let button = document.createElement("button");
     button.innerText = pokemon.name;
-    button.classList.add("button-class");
+    $(button).addClass('btn btn-outline-danger btn-width group-list-item');
+    $(button).attr({ 'data-toggle': 'modal', 'data-target': '#pokemon-modal' });
+
     listPokemon.appendChild(button);
     pokemonList.appendChild(listPokemon);
+
     button.addEventListener('click', function(event) {
       showDetails(pokemon);
+    });
+  }
+
+  /* Show modal called when clicked */
+  function showModal(item) {
+  loadDetails(item).then(function() {
+      let modalTitle = $('.modal-title');
+      let modalBody = $('.modal-body');
+      let columnLeft = $('.col__left');
+      let modalRow = $('.modal-row');
+
+      // $(modalTitle).empty();
+      // $(modalBody).empty();
+      // $(columnCenter).empty();
+      // $(modalRow).empty();
+
+      let nameElement = $('<h1>' + item.name + '</h1>');
+      let heightElement = $('<div>' + '<p>' + ' Height: ' + item.height + '</p>' + '</div>');
+      let typeElement = $('<div>' + '<p>' + ' Type: ' + item.types + '</p>' + '</div>');
+      let abilitiesElement = $('<div>' + '<p>' + 'Abilities: ' + item.abilities + '</p>' + '</div>');
+
+      let imageElement = $('<img class="modal-image">');
+
+      imageElement.attr('src', item.imageUrl);
+      imageElement.attr('id', 'modal-image');
+
+      modalTitle.append(nameElement);
+      columnLeft.append(heightElement, imageElement);
+      modalRow.append(columnLeft);
+      modalBody.append(modalRow);
     });
   }
 
